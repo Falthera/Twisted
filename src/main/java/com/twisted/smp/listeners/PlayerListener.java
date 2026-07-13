186fef9cac1e5eeebaed38238187740811d0bb69
falthera <falthera@users.noreply.github.com>
2026-07-13 14:00:28 +1000

    fix: only consume chorus fruit if ability actually fires

--- src/main/java/com/twisted/smp/listeners/PlayerListener.java
+++ src/main/java/com/twisted/smp/listeners/PlayerListener.java
@@ -265,12 +265,13 @@ public class PlayerListener implements Listener {
         }
         if (abilityMat != null && item.getType() == abilityMat) {
             event.setCancelled(true);
-            if (item.getAmount() > 1) {
-                item.setAmount(item.getAmount() - 1);
-            } else {
-                player.getInventory().remove(item);
+            if (plugin.getAbilityManager().useAbility(player, data.getTwist())) {
+                if (item.getAmount() > 1) {
+                    item.setAmount(item.getAmount() - 1);
+                } else {
+                    player.getInventory().remove(item);
+                }
             }
-            plugin.getAbilityManager().useAbility(player, data.getTwist());
             return;
         }
     }
