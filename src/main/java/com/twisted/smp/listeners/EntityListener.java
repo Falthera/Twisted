218f530e61c9a85617fdf983adb4ae8060d33cfd
falthera <falthera@users.noreply.github.com>
2026-07-13 13:33:11 +1000

    fix: correct EntityDamageEvent import package

--- src/main/java/com/twisted/smp/listeners/EntityListener.java
+++ src/main/java/com/twisted/smp/listeners/EntityListener.java
@@ -11,7 +11,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityPortalEnterEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
